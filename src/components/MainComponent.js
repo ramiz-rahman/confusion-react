e1e8ac72bed537ed6b24c423655a855ca0ddeb91
ramiz-rahman <ramiz.rahman@outlook.com>
2018-12-31 21:19:45 +0600

    Assignment 2, Task 2 & 3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -45,7 +45,7 @@ class Main extends Component {
             )[0]
           }
           comments={this.state.comments.filter(
-            comment => comment.dishId == parseInt(match.params.dishId, 10)
+            comment => comment.dishId === parseInt(match.params.dishId, 10)
           )}
         />
       );
